091af5aa5ce048fe0746bd30ba8b952bc2a1e83a
Dakarai Simmons <infinitelyCannon@gmail.com>
2019-03-23 23:12:19 -0400

    First push for prototype

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CameraController{
+    public float YSensitivity = 1f;
+    public float xSpeed = 1f;
+    public float distance;
+    public bool invertedX = true;
+    public bool lockCursor = true;
+
+    private Vector3 mCharacterPosition;
+    private Vector3 mCameraPosition;
+    private Vector2 mUnitPosition;
+    private float yOffset;
+    private Transform mCamera;
+    private bool mCursorLocked = true;
+
+    public void Init(Transform character, Transform camera)
+    {
+        Vector3 unit3D = camera.position.normalized;
+        mCameraPosition = camera.position;
+        mCharacterPosition = character.position;
+        mUnitPosition = new Vector2(unit3D.x, unit3D.z);
+        //distance = 10f;//camera.position.magnitude;
+        yOffset = camera.position.y;
+        mCamera = camera;
+    }
+
+    public void UpdatePosition()
+    {
+        float horizontal = Input.GetAxisRaw("Mouse X") * xSpeed;
+        float vertical = Input.GetAxisRaw("Mouse Y") * YSensitivity;
+
+        horizontal *= invertedX ? -1f : 1f;
+
+        mCamera.Rotate(Vector3.up, horizontal * Time.deltaTime);
+        /*
+        Vector2 tangent;
+
+        mUnitPosition = new Vector2(Mathf.Cos(horizontal), Mathf.Sin(horizontal));
+        tangent = FindTangent(mUnitPosition);
+        if(horizontal != 0f)
+        {
+            mCamera.position += new Vector3(tangent.x, 0f, tangent.y);
+            //Debug.Log(tangent);
+        }
+        */
+    }
+
+    public void SetCursorLock(bool value)
+    {
+        lockCursor = value;
+        if (!lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    public void UpdateCursorLock()
+    {
+        if (lockCursor)
+            InternalLockUpdate();
+    }
+
+    private void InternalLockUpdate()
+    {
+        if (Input.GetKeyUp(KeyCode.L))
+            mCursorLocked = false;
+        else if (Input.GetMouseButtonUp(0))
+            mCursorLocked = true;
+
+        if (mCursorLocked)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField] private float walkSpeed = 5f;
+    [SerializeField] private float runSpeed = 10f;
+    [SerializeField] private float jumpSpeed = 10f;
+    [SerializeField] private float stickToGroundForce = 9.8f;
+    [SerializeField] private float gravityMultiplyer = 2f;
+    [SerializeField] private CameraController cameraController;
+
+    private Camera mCamera;
+    private bool jump;
+    private Vector2 mInput;
+    private Vector3 moveDir = Vector3.zero;
+    private Vector3 mForward;
+    private CharacterController characterController;
+    private CollisionFlags collisionFlags;
+    private bool previouslyGrounded;
+    private bool jumping;
+    private bool isWalking;
+
+    // Use this for initialization
+    void Start () {
+        characterController = GetComponent<CharacterController>();
+        mCamera = Camera.main;
+        jumping = false;
+        cameraController.Init(transform, mCamera.transform.parent);
+        mForward = mCamera.transform.forward;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            //Debug.Log(mCamera.transform.parent);
+        }
+
+        cameraController.UpdatePosition();
+        cameraController.UpdateCursorLock();
+        mForward = mCamera.transform.forward;
+
+        if (!jump && !jumping)
+            jump = Input.GetButtonDown("Jump");
+
+        if(!previouslyGrounded && characterController.isGrounded)
+        {
+            moveDir.y = 0f;
+            jumping = false;
+        }
+
+        if (!characterController.isGrounded && !jumping && previouslyGrounded)
+            moveDir.y = 0f;
+
+        previouslyGrounded = characterController.isGrounded;
+	}
+
+    private void FixedUpdate()
+    {
+        float speed;
+        GetInput(out speed);
+
+        Vector3 desiredMove = mForward * mInput.y + transform.right * mInput.x;
+
+        RaycastHit hitInfo;
+        Physics.SphereCast(transform.position, characterController.radius, Vector3.down, out hitInfo, characterController.height / 2f, Physics.AllLayers, QueryTriggerInteraction.Ignore);
+        desiredMove = Vector3.ProjectOnPlane(desiredMove, hitInfo.normal).normalized;
+
+        moveDir.x = desiredMove.x * speed;
+        moveDir.z = desiredMove.z * speed;
+
+        if (characterController.isGrounded)
+        {
+            moveDir.y = -stickToGroundForce;
+
+            if (jump)
+            {
+                moveDir.y = jumpSpeed;
+                jump = false;
+                jumping = true;
+            }
+        }
+        else
+        {
+            moveDir += Physics.gravity * gravityMultiplyer * Time.fixedDeltaTime;
+        }
+
+        collisionFlags = characterController.Move(moveDir * Time.fixedDeltaTime);
+    }
+
+    private void GetInput(out float speed)
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        isWalking = !Input.GetKey(KeyCode.LeftShift);
+
+        speed = isWalking ? walkSpeed : runSpeed;
+
+        mInput = new Vector2(horizontal, vertical);
+
+        if (mInput.sqrMagnitude > 1f)
+            mInput.Normalize();
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody body = hit.collider.attachedRigidbody;
+        if (collisionFlags == CollisionFlags.Below)
+            return;
+        if (body == null || body.isKinematic)
+            return;
+
+        body.AddForceAtPosition(characterController.velocity * 0.1f, hit.point, ForceMode.Impulse);
+    }
+}
