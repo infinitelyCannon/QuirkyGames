343c197eef738b74d7c243abfdb47e7a8687843e
KyleLambdin78 <43045070+KyleLambdin78@users.noreply.github.com>
2019-03-24 22:02:22 -0400

    Add files via upload
    Enemy script for basic follow and retreat

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+
+    public float speed;
+    public float stopDistance;
+    public float retreatDistance;
+
+    public Transform player;
+
+	// Use this for initialization
+	void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Vector3.Distance(transform.position, player.position) > stopDistance)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }
+        else if (Vector3.Distance(transform.position, player.position) < stopDistance && Vector3.Distance(transform.position, player.position) > retreatDistance)
+        {
+            transform.position = this.transform.position;
+        }
+        else if (Vector3.Distance(transform.position, player.position) < retreatDistance)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+        }
+    }
+
+
+
+}
+
