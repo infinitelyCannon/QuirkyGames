dcf208bb96a2069e99cc6497db6ec959952b35f0
maxstertko <33167611+maxstertko@users.noreply.github.com>
2019-04-21 22:58:39 -0400

    Add files via upload

---
+++ Assets/Scripts/EnemyDeathScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDeathScript : MonoBehaviour {
+    public GameObject malfunction;
+
+
+
+	// Use this for initialization
+	void Start () {
+        Explode();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    //Disable enemy movement on death then have them fade out
+    public void Explode() {
+        if(malfunction != null) {
+            GameObject malfunctionFx = Instantiate(malfunction, transform.position, Quaternion.identity);
+            Destroy(malfunctionFx, 5);
+        }
+    }
+}
