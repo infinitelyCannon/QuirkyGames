1a2d3d958036f67c101acc58034cf4b285190952
Dakarai Simmons <infinitelyCannon@gmail.com>
2019-04-21 23:51:38 -0400

    Added dialog type ability to audio script trigger

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -1,17 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Dialogue : MonoBehaviour
 {
     private AudioSource mAudioSource;
     public AudioClip dialogue;
+    public float typeDelay;
+    public float lifeTime;
+    [TextArea] public string script;
+
     private bool playing;
+    private Text dialogText;
+    private string textBuffer = "";
+    private delegate void AfterEffect();
 
     private void Start()
     {
         mAudioSource = GetComponent<AudioSource>();
         playing = false;
+        dialogText = GameObject.Find("DialogText").GetComponent<Text>();
     }
     void OnTriggerEnter(Collider other)
     {
@@ -19,6 +28,32 @@ public class Dialogue : MonoBehaviour
         {
             mAudioSource.PlayOneShot(dialogue);
             playing = true;
+            StartCoroutine(Type(0f, script, lifeTime, () => { Clear(); }));
         }
+    }
+
+    private void Clear()
+    {
+        dialogText.text = dialogText.text.Replace(script, "");
+    }
+
+    IEnumerator Type(float startDelay, string message, float endDelay, AfterEffect action)
+    {
+        char[] temp = dialogText.text.ToCharArray();
+        int msgIdx = 0, lastIdx = 0;
+
+        if (startDelay > 0f)
+            yield return new WaitForSeconds(startDelay);
+
+        while(msgIdx < message.Length)
+        {
+            dialogText.text = dialogText.text.Insert(lastIdx++, message.Substring(msgIdx++, 1));
+            yield return new WaitForSeconds(typeDelay);
+        }
+
+        if (endDelay > 0f)
+            yield return new WaitForSeconds(endDelay);
+
+        action?.Invoke();
     }
 }
